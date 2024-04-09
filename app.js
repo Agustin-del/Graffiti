292f344fb2e4e72f3a4abec78c708d12c3973aa9
Agustin-del <eddyruveda@gmail.com>
2024-04-09 09:52:34 -0300

    'fin'

--- app.js
+++ app.js
@@ -11,15 +11,42 @@ colorInput.addEventListener("change", (event) => color = event.target.value)
 // grafiti.lastChild.classList.add(`bg-[${event.target.value}]`)
 
 boton.addEventListener('click', (event) => {
+  
   event.preventDefault()
-  grafiti.innerHTML += `<div class="text-xl flex flex-wrap break-all items-center justify-center w-[200px] h-[200px] p-4 bg-[${color}]">${texto}</div>`
-  // grafiti.innerText = texto
+  grafiti.innerHTML += `<div class="poster text-xl flex flex-row-reverse break-all w-[200px] h-[200px] p-4 bg-[${color}]">
+  <span class ="close w-[20px] h-[20px]"><img class = "h-[20px] object-contain" src="./cross.svg"></span>
+  <h2 class ="self-center w-full justify-self-center">${texto}</h2>
+  </div>`
+
+  let close = document.getElementsByClassName('close')
+  for (const tag of close) {
+      tag.addEventListener('click', event =>{
+        grafiti.innerHTML = ''
+      })
+  }
+
+  let contador = 0
+  let poster = document.getElementsByClassName('poster')
+  let style = document.querySelectorAll('input[name="style"]')
+  
+  for (const element of style) {
+    element.addEventListener('change', event =>{
+      console.log(event.target.value)
+      if(event.target.value === 'grafitty') {
+        grafiti.children[contador].classList.add('italic')
+      } else {
+        grafiti.children[contador].classList.remove('italic')
+      }
+    })
+  }
+  contador += 1
 })
 
+// grafiti.innerText = texto
 
 
 
-
+//bueno ya es re tarde y no me estoy dando cuenta como hago para aplicar el italic y el eliminar a sólo el elemento seleccionado, je..
 
 
 
